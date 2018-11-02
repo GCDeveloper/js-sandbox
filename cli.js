02f5e3c466379cdeed450a7171f8b57e63caa8e3
George Campbell <campbell.goe@gmail.com>
2018-11-02 19:03:22 +0000

    Add files via upload

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
 const arg = process.argv[2];
 const startMessage = "Hello";
+const cliArgs = process.argv.slice(2);
 console.log(startMessage, arg ? arg : "");
+console.log("arguments:\n", cliArgs);
