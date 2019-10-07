2a178b306062a80b8c84fcdc8882dec79e613255
George Campbell <campbell.goe@gmail.com>
2019-10-07 19:24:47 +0100

    simplified 'all.js'

--- functions/all.js
+++ functions/all.js
@@ -1,8 +1,6 @@
 //this function does something to all arguments after cb and start
 //and is an example of functional programming
-function all(cb, initialValue) {
-  return Array.from(arguments)
-    .slice(2)
-    .reduce(cb, initialValue);
+function all(cb, initialValue, ...args) {
+  return args.reduce(cb, initialValue);
 }
 module.exports = all;
